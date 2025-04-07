dbcd486c8a8f252e2404f3ac5d3a51dc3ac40ec0
KristinaTomash <kristitomsh@gmail.com>
2025-04-07 21:01:13 +0300

    Add14

--- src/test/java/com/phonebook/tests/TestBase.java
+++ src/test/java/com/phonebook/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.phonebook.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -32,7 +33,14 @@ public class TestBase {
         logger.info("Start test" + method.getName());
 }
 @AfterMethod
-    public void stopTest(){
+    public void stopTest(ITestResult result){
+        if(result.isSuccess()){
+            logger.info("PASSED: "+ result.getMethod().getMethodName());
+        }
+        else {
+            logger.error("FAILED: "+ result.getMethod().getMethodName()+" Screenshot path: "+ app.getUser().takeScreenshot());
+        }
         logger.info("Stop test");
+        logger.info("==================================================================");
 }
 }
